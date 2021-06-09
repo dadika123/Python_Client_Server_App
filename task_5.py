5406a615c5d3f9ed906dddbc4393c681c2c59275
chuzhikov_danil <danil.chuzhikov@gmail.com>
2021-06-09 18:59:27 +0300

    fix

--- task_5.py
+++ task_5.py
@@ -6,7 +6,7 @@ for target in targets_ping:
     if platform.system() == 'Windows':
         args = ['ping', target]
     else:
-        args = ['ping', '-c', 4, target]
+        args = ['ping', '-c 4',  target]
     subproc_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
     for line in subproc_ping.stdout:
         line = line.decode('cp866').encode('utf-8')
